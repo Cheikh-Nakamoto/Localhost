2589f6105c068fdd241d4d83c00d9b87b6ee4206
lodiouf <louisisaacdf@gmail.com>
2025-02-02 15:38:07 +0000

    Redirection OK.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -192,38 +192,42 @@ impl Server {
     ) -> Result<(), std::io::Error> {
         let mut redirects = self.redirections.clone();
         redirects.retain(|r| r.source == request.location);
+        let mut code = 302;
+        let mut status = "Loop Detected";
 
         if !redirects.is_empty() {
             if self.redirections.iter().any(|r| r.target == request.location) {
+                code = 508;
                 Self::send_error_response(
                     &self,
                     stream,
                     &request,
                     config,
-                    508,
-                    "Loop Detected",
+                    code,
+                    status,
                     &cookie
                 )?;
             } else {
-                // Construire la réponse de redirection
-                let response = format!(
-                    "HTTP/1.1 302 Found\r\n\
-                 Location: {}\r\n\
-Connection: keep-alive\r\n\
-                 Content-Length: 0\r\n\r\n",
-                    redirects[0].target.clone()
-                );
-
+                status = "Found";
                 request.location = redirects[0].target.clone();
                 let re = Regex::new(r"^(?<method>[A-Z]+) /(?<location>\S+)").unwrap();
                 request.head = re
                     .replace_all(&request.head, format!("$method {}", redirects[0].target.clone()))
                     .to_string();
-
-                // Envoyer la réponse via le TcpStream
-                stream.write_all(response.as_bytes())?;
-                stream.flush()?;
             }
+
+            // Construire la réponse de redirection
+            let response = format!(
+                "HTTP/1.1 {code} {status}\r\n\
+                Location: {}\r\n\
+                Connection: keep-alive\r\n\
+                Content-Length: 0\r\n\r\n",
+                request.location
+            );
+
+            // Envoyer la réponse via le TcpStream
+            stream.write_all(response.as_bytes())?;
+            stream.flush()?;
         }
 
         Ok(())
@@ -344,6 +348,7 @@ Connection: keep-alive\r\n\
                                                 file_formats.insert(".jpeg", "image");
                                                 file_formats.insert(".png", "image");
                                                 file_formats.insert(".txt", "text");
+                                                file_formats.insert(".pdf", "pdf");
 
                                                 match file_formats.get(ext.as_str()) {
                                                     Some(filetype) => filetype.to_string(),
